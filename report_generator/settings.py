f8a17a0f042b96ccc62f863d2f15f5ec11646aee
godot63 <lcalmbach@gmail.com>
2026-01-23 06:53:16 +0100

    Fix missing quotation mark in version_date in APP_INFO

--- report_generator/settings.py
+++ report_generator/settings.py
@@ -199,7 +199,7 @@ MARKDOWNIFY = {
 
 APP_INFO = {
     "version": "0.1.7",
-    "version_date": "2026-01-23,
+    "version_date": "2026-01-23",
     "author_name": "Lukas Calmbach",
     "author_email": "lcalmbach@gmail.com",
     "repo_url": "https://github.com/lcalmbach/open-data-insights",
